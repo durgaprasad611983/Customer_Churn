f118e3a889531903e90365f2c0170d83d33ae450
durgaprasad611983 <45446463+durgaprasad611983@users.noreply.github.com>
2026-08-06 22:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import pandas as pd
 import streamlit as st
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
 # -----------------------------------------------------------------
 # Page configuration

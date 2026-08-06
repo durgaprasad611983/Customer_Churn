9dcfb2e415c1f39370fccb0e730835e8ef1148de
durgaprasad611983 <45446463+durgaprasad611983@users.noreply.github.com>
2026-08-06 22:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ Run with:
 import pickle
 import numpy as np
 import pandas as pd
-import streamlit as st
+#import streamlit as st
 #import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
